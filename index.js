9aedc262e2fc225b93a4421813f95eec04c0df18
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-10-06 09:48:59 +0600

    server

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Dulal:dulal1234@cluster0.ml5ji.mongodb.net/volunteer?retryWrites=true&w=majority";
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology:true });
+client.connect(err => {
+  const collection = client.db("volunteer").collection("volunteerList");
+  //create data 
+  app.post('/addVolunteer',(req,res)=>{
+      const volunteer = req.body;
+      collection.insertOne(volunteer)
+      
+
+  })
+  //read
+  app.get('/volunterr',(req,res)=>{
+    
+    collection.find({email:req.query.email})
+    .toArray((err,documents)=>{
+     res.send(documents);
+    })
+  })
+  //delete
+  app.delete('/delete',(req,res)=>{
+    collection.deleteOne({id:req.query.id})
+    .then(result=>{
+      res.send(result.deletedCount>0)
+    })
+  })
+
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(5000);
